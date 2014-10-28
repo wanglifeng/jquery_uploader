3b7f76607954e4f818a81bf67c98bafa8e2b7f5f
wanglifeng <tytc011@163.com>
2014-10-29 00:48:26 +0800

    create a demo page

---
+++ MvcApplication3/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            return new JsonResult() { Data = String.Format("File Count:{0}", Request.Files.Count) };
+        }
+
+    }
+}
